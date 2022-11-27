731c8b3ef117c795efe4d782828c6fffd94ded1b
Karina <karina.ostrovsika@gmail.com>
2022-11-27 23:53:53 +0200

    hightlight messages card when messages menu item is clicked

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 // sidebar
 const menuItems = document.querySelectorAll('.menu-item');
+// messages
+const messagesNotification = document.querySelector('#messages-notifications');
+const messages = document.querySelector('.messages');
+const message = messages.querySelectorAll('.messages');
+const messageSearch = document.querySelectorAll('#message-search');
 
 // remove active class for all menu items
 const changeActiveItem = () =>{
@@ -16,7 +21,30 @@ menuItems.forEach(item => {
             document.querySelector('.notifications-popup').style.display = 'none';
         }else{
             document.querySelector('.notifications-popup').style.display = 'block';
-            document.querySelector()
+            document.querySelector('#notifications .notification-count').style.display = 'none';
         }
     })
+})
+
+// ---------------------------messages-----------------------------
+
+
+// search chart
+
+const searchMessage = () =>{
+    const val = messageSearch.values.toLowerCase();
+    console.log(val);
+    message.forEach(chat =>{
+        let name = chat.querySelector('h5').textContent.toLocaleLowerCase();
+    })
+}
+
+messageSearch.addEventListener('keyup',searchMessage);
+
+messagesNotification.addEventListener('click', () => {
+    messages.style.boxShadow = '0 0 1rem var(--color-primary)';
+    messagesNotification.querySelector('.notification-count').style.display = 'none';
+    setTimeout(() =>{
+        messages.style.boxShadow = 'none';
+    },2000);
 })
